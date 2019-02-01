160614668d7ff0886ca2eca3ffea64e2137495f5
nishant-ketu <47020252+nishant-ketu@users.noreply.github.com>
2019-02-01 11:30:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,29 +4,15 @@ from flask_dance.contrib.github import make_github_blueprint, github
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisissupposedtobeasecret'
-#twitter_blueprint = make_twitter_blueprint(api_key='API_KEY', api_secret='API_SECRET')
 
-github_blueprint = make_github_blueprint(client_id='cc39486b294af8437655', client_secret='6bcf1f60975d61a0b596e33a2c012016c630ce3b')
 
-#app.register_blueprint(twitter_blueprint, url_prefix='/twitter_login')
+github_blueprint = make_github_blueprint(client_id='CLIENT_ID', client_secret='CLIENT_SECRET')
+
+
 
 app.register_blueprint(github_blueprint, url_prefix='/github_login')
 
-'''@app.route('/twitter')
-def twitter_login():
-    if not twitter.authorized:
-        return redirect(url_for('twitter.login'))
-    account_info = twitter.get('account/settings.json')
 
-    if account_info.ok:
-        account_info_json = account_info.json()
-
-        return '<h1>Your Twitter name is @{}'.format(account_info_json['screen_name'])
-
-    return '<h1>Request failed!</h1>'
-    '''
-
-@app.route('/github')
 def github_login():
     if not github.authorized:
         return redirect(url_for('github.login'))
